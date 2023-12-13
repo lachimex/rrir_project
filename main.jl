269daa34040a9f128a9f200e02e2914c572918f7
Michał <dydmichal@gmail.com>
2023-12-13 14:59:31 +0100

    added fun

--- main.jl
+++ main.jl
@@ -10,21 +10,24 @@ function gauss_legendre_quadrature(degree)
     return nodes, weights
 end
 
-degree = 4  # Stopień kwadratury Gaussa-Legendre'a
-nodes, weights = gauss_legendre_quadrature(degree)
+function integral_on_a_b(fun, a, b)
+    degree = 10  # Stopień kwadratury Gaussa-Legendre'a
+    nodes, weights = gauss_legendre_quadrature(degree)
 
-# Wyświetlanie węzłów i wag
-println("Węzły: ", nodes')
-println("Wagi: ", weights')
+    # Wyświetlanie węzłów i wag
+    println("Węzły: ", nodes')
+    println("Wagi: ", weights')
 
-global result = 0.0
-global a = -1
-global b = 2
+    global result = 0.0
+    global a = -1
+    global b = 2
 
-for i in 1:length(nodes)
-    # Replace the following line with your function evaluation at nodes[i]
-    global result = result + f((a+b)/2 + ((b-a) * nodes[i]) / 2) * weights[i]
+    for i in 1:length(nodes)
+        # Replace the following line with your function evaluation at nodes[i]
+        global result = result + f((a+b)/2 + ((b-a) * nodes[i]) / 2) * weights[i]
+    end
+
+    println("Wynik: ", ((b-a)/2) * result)
+    return result
 end
-
-println("Wynik: ", ((b-a)/2) * result)
 
