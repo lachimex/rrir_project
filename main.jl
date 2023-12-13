c1fd6bb3496aaabbc7cd3afca7c8673e42c00122
Michał Dydek <dydmichal@gmail.com>
2023-12-13 13:08:33 +0100

    changes from fixed a and b to any a and b

--- main.jl
+++ main.jl
@@ -18,11 +18,13 @@ println("Węzły: ", nodes')
 println("Wagi: ", weights')
 
 global result = 0.0
+global a = -1
+global b = 2
 
 for i in 1:length(nodes)
     # Replace the following line with your function evaluation at nodes[i]
-    global result = result + f(nodes[i]) * weights[i]
+    global result = result + f((a+b)/2 + ((b-a) * nodes[i]) / 2) * weights[i]
 end
 
-println("Wynik: ", result)
+println("Wynik: ", ((b-a)/2) * result)
 
