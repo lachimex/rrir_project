acf6293b7083d303a17598568b4dd71bbd8c5f75
Michał <dydmichal@gmail.com>
2023-12-13 18:04:02 +0100

    B and L defs

--- main.jl
+++ main.jl
@@ -1,9 +1,9 @@
 using LinearAlgebra
 using QuadGK
-using Math
 using ForwardDiff
 
 const G = 6.67430e-11
+const h = 1e-8
 
 function f(x)
     return x * x
@@ -18,10 +18,6 @@ function integral_on_a_b(fun, a, b)
     degree = 10  # Stopień kwadratury Gaussa-Legendre'a
     nodes, weights = gauss_legendre_quadrature(degree)
 
-    # Wyświetlanie węzłów i wag
-    println("Węzły: ", nodes')
-    println("Wagi: ", weights')
-
     global result = 0.0
     for i in 1:length(nodes)
         # Replace the following line with your function evaluation at nodes[i]
@@ -31,11 +27,34 @@ function integral_on_a_b(fun, a, b)
     return ((b-a)/2) * result
 end
 
+function integral_on_a_b_v2(fun1, fun2, a, b)
+    degree = 10  # Stopień kwadratury Gaussa-Legendre'a
+    nodes, weights = gauss_legendre_quadrature(degree)
+
+    global result = 0.0
+    for i in 1:length(nodes)
+        x = (a+b)/2 + ((b-a) * nodes[i]) / 2
+        global result = result + ForwardDiff.derivative(fun1, x) * ForwardDiff.derivative(fun2, x) * weights[i]
+    end
+
+    return ((b-a)/2) * result
+end
+
 function L(v)
-    return 4 * pi * G * integral_on_a_b(v, 1, 2);
+    return 4 * π * G * integral_on_a_b(v, 1, 2);
 end
 
 function B(w, v)
-    return -1 * integral_on_a_b(ForwardDiff.derivative(w) * ForwardDiff.derivative(v), 0, 3);
+    return -1 * integral_on_a_b_v2(w, v, 0, 3);
 end
+
+function f1(x)
+    return x * x
+end
+
+function f2(x)
+    return x * x
+end
+
+println(B(f1, f2))
 
