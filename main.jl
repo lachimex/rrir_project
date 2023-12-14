06ea7cee472ba5ca356c76acdef1f70a13b7f3e8
Michał <dydmichal@gmail.com>
2023-12-14 18:24:15 +0100

    solving matrix equation

--- main.jl
+++ main.jl
@@ -9,20 +9,29 @@ function f(x)
     return x * x
 end
 
+function ro(x)
+    if (1 < x && x <= 2)
+        return 1
+    else
+        return 0
+    end
+end
+
 function integral_on_a_b(fun, a, b)
-    degree = 10
+    degree = 50
     nodes, weights = gauss(Float64, degree)
 
     global result = 0.0
     for i in 1:length(nodes)
-        global result = result + fun((a+b)/2 + ((b-a) * nodes[i]) / 2) * weights[i]
+        x = (a+b)/2 + ((b-a) * nodes[i]) / 2
+        global result = result + ro(x) * fun(x) * weights[i]
     end
 
     return ((b-a)/2) * result
 end
 
 function integral_on_a_b_v2(fun1, fun2, a, b)
-    degree = 10
+    degree = 50
     nodes, weights = gauss(Float64, degree)
 
     global result = 0.0
@@ -35,20 +44,55 @@ function integral_on_a_b_v2(fun1, fun2, a, b)
 end
 
 function L(v)
-    return 4 * π * G * integral_on_a_b(v, 1, 2);
+    return integral_on_a_b(v, 0, 3);
 end
 
 function B(w, v)
     return -1 * integral_on_a_b_v2(w, v, 0, 3);
 end
 
-function f1(x)
-    return x * x
+n = 2
+
+function e_i_odd(n, i)
+    dx = 3 / n
+    x_1 = dx * div(i, 2)
+    x_2 = x_1 + dx
+    a = 1/(x_1 - x_2)
+    b = 1 - x_1/(x_1 - x_2)
+    return function (x)
+        a * x + b
+    end
 end
 
-function f2(x)
-    return x * x
+function e_i_even(n, i)
+    dx = 3 / n
+    x_1 = dx * div(i, 2)
+    x_2 = x_1 + dx
+    a = 1/(x_2 - x_1)
+    b = 1 - x_2/(x_2 - x_1)
+    return function (x)
+        a * x + b
+    end
 end
 
-println(B(f1, f2))
+A = zeros(Float64, 2*n, 2*n)
+C = zeros(Float64, 2*n, 1)
 
+for i in 1:2*n
+    if i % 2 == 0
+        e_i = e_i_even(n, i)
+    else
+        e_i = e_i_odd(n, i)
+    end
+    for j in 1:2*n
+        if j % 2 == 0
+            e_j = e_i_even(n, j)
+        else
+            e_j = e_i_odd(n, j)
+        end
+        A[i, j] = B(e_i, e_j)
+    end
+    C[i] = L(e_i)
+end
+
+println(C \ A)
